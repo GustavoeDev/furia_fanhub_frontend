991e404ae1c469287897053203134932b25a1d96
GustavoeDev <gustavoe.dev@gmail.com>
2025-05-04 11:56:28 -0300

    feat: add LastMatches component and integrate embla-carousel for displaying recent matches

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import BannerMain from "@/components/sections/banner";
+import LastMatches from "@/components/sections/last-matches";
 import LiveMatch from "@/components/sections/live-match";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <main>
       <BannerMain />
       <LiveMatch />
+      <LastMatches />
     </main>
   );
 }
---
+++ src/components/sections/last-matches.tsx
@@ -0,0 +1,86 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import Image from "next/image";
+
+import competition from "@/assets/esl-challenger.jpg";
+
+export default function LastMatches() {
+  return (
+    <div className="flex flex-col gap-8 mb-10 my-30">
+      <div>
+        <h2 className="text-2xl font-bold">Últimas partidas</h2>
+      </div>
+      <Carousel className="mx-14">
+        <CarouselContent className="items-stretch min-h-[300px]">
+          <CarouselItem className="basis-1/3">
+            <div className="relative w-full h-[300px]">
+              <Image
+                src={competition}
+                alt="Foto do campeonato"
+                fill
+                className="object-cover rounded"
+              />
+            </div>
+          </CarouselItem>
+
+          <CarouselItem className="basis-1/3">
+            <div className="relative w-full h-[300px]">
+              <Image
+                src={competition}
+                alt="Foto do campeonato"
+                fill
+                className="object-cover rounded"
+              />
+            </div>
+          </CarouselItem>
+
+          <CarouselItem className="basis-1/3">
+            <div className="relative w-full h-[300px]">
+              <Image
+                src={competition}
+                alt="Foto do campeonato"
+                fill
+                className="object-cover rounded"
+              />
+            </div>
+          </CarouselItem>
+
+          <CarouselItem className="basis-1/3">
+            <div className="relative w-full h-[300px]">
+              <Image
+                src={competition}
+                alt="Foto do campeonato"
+                fill
+                className="object-cover rounded"
+              />
+            </div>
+          </CarouselItem>
+
+          <CarouselItem className="basis-1/3">
+            <div className="relative w-full h-[300px]">
+              <Image
+                src={competition}
+                alt="Foto do campeonato"
+                fill
+                className="object-cover rounded"
+              />
+            </div>
+          </CarouselItem>
+        </CarouselContent>
+        <CarouselPrevious
+          variant="secondary"
+          className="bg-[#C49D2B] hover:bg-[#c49e2b93] cursor-pointer"
+        />
+        <CarouselNext
+          variant="secondary"
+          className="bg-[#C49D2B] hover:bg-[#c49e2b93] cursor-pointer"
+        />
+      </Carousel>
+    </div>
+  );
+}
