3226e2424a22b9bf01e9679452a57bf2360ebe3b
GustavoeDev <gustavoe.dev@gmail.com>
2025-05-05 18:51:36 -0300

    feat: display completed matches in carousel and handle empty state

--- src/components/sections/last-matches.tsx
+++ src/components/sections/last-matches.tsx
@@ -9,34 +9,51 @@ import {
 } from "@/components/ui/carousel";
 import Game from "../shared/game";
 import { useTeamStore } from "@/stores/team-store";
+import { useEffect, useState } from "react";
+import { Match } from "@/types/match";
 
 export default function LastMatches() {
   const { team } = useTeamStore();
+  const [matchesCompleted, setMatchesCompleted] = useState<Match[]>([]);
+
+  useEffect(() => {
+    const teamMatch = team.matches?.filter(
+      (match) => match.status === "completed"
+    );
+
+    if (teamMatch) {
+      setMatchesCompleted(teamMatch);
+    }
+  }, [team]);
 
   return (
     <div className="flex flex-col gap-8 my-30">
       <div>
         <h2 className="text-2xl font-bold font-title">Últimas partidas</h2>
       </div>
-      <Carousel className="mx-14">
-        <CarouselContent className="items-stretch min-h-[300px]">
-          {team.matches?.slice(0, 5).map((match) => {
-            return (
-              <CarouselItem key={match.id} className="basis-1/3">
+      {matchesCompleted.length > 0 ? (
+        <Carousel className="mx-14">
+          <CarouselContent className="items-stretch min-h-[300px]">
+            {matchesCompleted.slice(0, 5).map((match) => (
+              <CarouselItem key={match.id} className="basis-1/3 completed">
                 <Game match={match} />
               </CarouselItem>
-            );
-          })}
-        </CarouselContent>
-        <CarouselPrevious
-          variant="secondary"
-          className="bg-[#C49D2B] hover:bg-[#c49e2b93] cursor-pointer"
-        />
-        <CarouselNext
-          variant="secondary"
-          className="bg-[#C49D2B] hover:bg-[#c49e2b93] cursor-pointer"
-        />
-      </Carousel>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious
+            variant="secondary"
+            className="bg-[#C49D2B] hover:bg-[#c49e2b93] cursor-pointer"
+          />
+          <CarouselNext
+            variant="secondary"
+            className="bg-[#C49D2B] hover:bg-[#c49e2b93] cursor-pointer"
+          />
+        </Carousel>
+      ) : (
+        <div>
+          <p>Não há partidas finalizadas para mostrar.</p>
+        </div>
+      )}
     </div>
   );
 }
